2e40939158eb547d09a67f74f612333286f4e4eb
Alberto <albertocp141@gmail.com>
2017-12-07 19:18:28 +0100

    Added multiple rigs support and rig status alert

---
+++ config.py
@@ -0,0 +1,9 @@
+token = "" #Token given by BotFather
+
+my_id =  #Your telegram id (You can get it from https://telegram.me/my_id_bot)
+
+rigs = [["Rig 0","192.168.1.100",3333], ["Rig 1","192.168.1.101",3333]] #Example  [["Rig 0","192.168.1.100",3333], ["Rig 1","192.168.1.101",3333]]
+
+# rigs = [["Name","IP",Port]]
+#IP of the rig (localhost if running bot in the rig)
+#Port where claymore's miner is listening (default 3333)
